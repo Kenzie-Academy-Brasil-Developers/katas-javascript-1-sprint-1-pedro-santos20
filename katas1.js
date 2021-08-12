a3f67affd3f7e916d7acc09a97ad74219ba3ad18
pedro santos <ps3979187@gmail.com>
2021-08-12 11:00:29 -0300

    Terminado todas as modificações de retorno das funções

--- katas1.js
+++ katas1.js
@@ -1,94 +1,106 @@
 function oneThroughTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result*/
-    
+  let numeros = []
+  for (let i = 1; i <= 20; i++) {
+    console.log(numeros + i)
+  }
+  return numeros
 }
-
+console.log(oneThroughTwenty())
 //call function oneThroughTwenty
 
 function evensToTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result */
-  
+  let pares = 2
+  while (pares < 20) {
+    console.log(pares)
+    pares += 2
+  }
+  return pares
 }
-
+console.log(evensToTwenty())
 //call function evensToTwenty
 
 function oddsToTwenty() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
-   
+  let impares = 1
+  while (impares < 19) {
+    console.log(impares)
+    impares += 2
+  }
+  return impares
 }
-
+console.log(oddsToTwenty())
 //call function oddsToTwenty
 
 function multiplesOfFive() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
-
+  let multiplo = 5
+  while (multiplo < 100) {
+    console.log(multiplo)
+    multiplo += 5
+  }
+  return multiplo
 }
 
 //call function multiplesOfFive
 
 function squareNumbers() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
-    
+  let quadrado = []
+  for (let i = 0; i < quadrado.length; i++) {
+    console.log(quadrado + i)
+    quadrado = i * i 
+  }
+  return quadrado
 }
-
+console.log(squareNumbers())
 //call function squareNumbers
 
 function countingBackwards() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  let numVolta = []
+  for (count = 20; count >= 1; count--) {
+    console.log(numVolta + count)
+  }
+  return numVolta
 }
-
+console.log(countingBackwards())
 //call function countingBackwards
 
 function evenNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  let parVolta = 20
+  while (parVolta > 2) {
+    console.log(parVolta)
+    parVolta -= 2
+  }
+  return parVolta
 }
-
+console.log(evenNumbersBackwards())
 //call function evenNumbersBackwards
 
 function oddNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  let imparVolta = 19
+  while (imparVolta > 1) {
+    console.log(imparVolta)
+    imparVolta -= 2
+  }
+  return imparVolta
 }
-
+console.log(oddNumbersBackwards())
 //call function oddNumbersBackwards
 
 function multiplesOfFiveBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  let multiploVolta = 100
+  while (multiploVolta > 5) {
+    console.log(multiploVolta)
+    multiploVolta -= 5
+  }
+  return multiploVolta
 }
-
+console.log(multiplesOfFiveBackwards())
 //call function multiplesOfFiveBackwards
 
 function squareNumbersBackwards() {
-    
-   /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  let quadradoVolta = []
+  for (let index = 0; index < quadradoVolta.length; index++) {
+    const element = quadradoVolta[index];
+  }
+  return quadradoVolta
 }
-
+console.log(squareNumbersBackwards())
 //call function squareNumbersBackwards
